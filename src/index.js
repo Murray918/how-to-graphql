dae8d5ab07ca94c1d78c90106c3ced7e3f509260
Andrew Murray <darbyguitars@gmail.com>
2019-11-11 17:54:09 -0600

    import the prisma client library for connecting our javascript to the prisma api removed the code that revolved around a in memory datastructure and replaced it with the new code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require('./generated/prisma-client')
 
-//describe the links
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
 
-let idCount = links.length;
+
 
 //resolvers there must be a resolver for each field
 const resolvers = {
   Query: {
     info: () => "This is the API of a Hackernews Clone.",
-    feed: () => links,
+    feed: () => (root, args, context, info) => {
+        return context.prisma.links()
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context) => {
+      return context.prisma.createdLink({
+          url: args.url,
+          description: args.description
+      })
     },
   }
 };
@@ -33,7 +25,8 @@ const resolvers = {
 //server
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context : { prisma }
 });
 
 server.start(() =>
