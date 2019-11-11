5817ea8b1c11d17070cf253727239c5e2c9d2f7f
Andrew Murray <darbyguitars@gmail.com>
2019-11-11 17:23:24 -0600

    setting every thing up to connect a database and setting every thing up for prisma via sql

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
 
 //describe the links
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+let idCount = links.length;
 
 //resolvers there must be a resolver for each field
 const resolvers = {
-    Query: {
-        info : () => 'This is the API of a Hackernews Clone.',
-        feed: () => links,
+  Query: {
+    info: () => "This is the API of a Hackernews Clone.",
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
     },
-    Link: {
-        id: (parent) => parent.id,
-        description : (parent) => parent.description,
-        url: (parent) => parent.url
-    }
-}
+  }
+};
 
 //server
 const server = new GraphQLServer({
-    typeDefs : './src/schema.graphql',
-    resolvers
-})
+  typeDefs: "./src/schema.graphql",
+  resolvers
+});
 
-server.start(() => console.log('your Server is running on http://localhost:4000'))
+server.start(() =>
+  console.log("your Server is running on http://localhost:4000")
+);
