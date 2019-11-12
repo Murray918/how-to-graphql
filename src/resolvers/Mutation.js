e240c4dae232557324c39a68c9e622c57858a2be
Andrew Murray <darbyguitars@gmail.com>
2019-11-11 20:07:17 -0600

    added the vote to the link so we can look up links by their votes	modified:   src/resolvers/Link.js added the vote to the mutations						modified:   src/resolvers/Mutation.js

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
-
 async function signup(parent, args, context, info) {
 	// salt and hash the password with bcrypt
 	const password = await bcrypt.hash(args.password, 10)
@@ -43,20 +42,43 @@ async function login(paren, args, context, info) {
 	}
 }
 
-function post(parent, args, context, info ) {
-    // get the user id for creating a link
-    const userId = getUserId(context)
+function post(parent, args, context, info) {
+	// get the user id for creating a link
+	const userId = getUserId(context)
 
-    // return the link with connected user
-    return context.prisma.createLink({
-        url: args.url,
-        description: args.description,
-        postedBy: { connect : { id : userId}}
+	// return the link with connected user
+	return context.prisma.createLink({
+		url: args.url,
+		description: args.description,
+		postedBy: { connect: { id: userId } }
+	})
+}
+
+async function vote(parent, args, context, info) {
+    // validate the jwt
+	const userId = getUserId(context)
+
+    // see if the link exist
+	const linkExists = await context.prisma.$exists.vote({
+		user: { id: userId },
+		link: { id: args.linkId }
+    })
+    
+    // if the link exists throw an error
+    if(linkExists) {
+        throw new Error('You have already voted for this link.')
+    }
+
+    // if not create the new vote with the user and the link
+    return context.prisma.createVote({
+        user: { connect: { id: userId } },
+        link: { connect: { id: args.linkId } } 
     })
 }
 
 module.exports = {
 	signup,
 	login,
-	post
+    post,
+    vote
 }
