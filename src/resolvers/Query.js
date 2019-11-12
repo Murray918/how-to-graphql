974ef6262c95b7d670ac6f4681231611f4e77301
Andrew Murray <darbyguitars@gmail.com>
2019-11-11 20:55:28 -0600

    added the pagination to the feed		modified:   src/resolvers/Query.js added the pagination to the feed query		modified:   src/schema.graphql

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,21 +1,24 @@
- async function feed(parent, args, context, info) {
-    // set up the filter
-    const where = args.filter ? {
-        OR: [
-            { description_contains: args.filter },
-            { url_contains: args.filter }
-        ]
-    } : {} // if no filter is provided we will set as empty object
+async function feed(parent, args, context, info) {
+	// set up the filter
+	const where = args.filter
+		? {
+				OR: [
+					{ description_contains: args.filter },
+					{ url_contains: args.filter }
+				]
+		  }
+		: {} // if no filter is provided we will set as empty object
 
-    //set up links with the filter
-    const links = await context.prisma.links({
-        where
-    })
+	//set up links with the filter
+	const links = await context.prisma.links({
+		where,
+		skip: args.skip,
+		first: args.first
+	})
 
-    return links
-  }
-  
-  module.exports = {
-    feed,
-  }
-  
+	return links
+}
+
+module.exports = {
+	feed
+}
