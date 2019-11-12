e319548fc7f5a018e71d956d16f646ab2f4522c6
Andrew Murray <darbyguitars@gmail.com>
2019-11-11 20:52:01 -0600

    changed the feet function into an async funciton

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,18 @@
-function feed(parent, args, context, info) {
-    return context.prisma.links()
+ async function feed(parent, args, context, info) {
+    // set up the filter
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter }
+        ]
+    } : {} // if no filter is provided we will set as empty object
+
+    //set up links with the filter
+    const links = await context.prisma.links({
+        where
+    })
+
+    return links
   }
   
   module.exports = {
